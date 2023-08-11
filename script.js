dec243f6085299f13abd63a72b1dbe1af2190781
Andres Choque <andreschoque71@gmail.com>
2023-08-11 18:39:28 -0400

    more edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//idk remember 
+// About section functionality
 
 let tablinks = document.getElementsByClassName("tab-links");
 let tabconts = document.getElementsByClassName("tab-conts");
@@ -17,6 +17,8 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("act-tab");
 }
 
+// Responsive view for menu 'burger' 
+
 let menus = document.getElementById("menus");
 
 function openmenu(){
@@ -26,3 +28,23 @@ function openmenu(){
 function closemenu() {
     menus.style.right = "-200px";
 }
+
+// Google Sheets API inspired from https://github.com/jamiewilson/form-to-google-sheets.git
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyiMFJxNG-Oe1735pct_MGwOk1EhP7t5LKAu9u4KHLdk_NqJIPJiSrXQrbs6Lzco8j7/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  const submitMsg = document.getElementById("submit-msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => { 
+        submitMsg.innerHTML = "Message Submitted"
+        setTimeout(function() {
+            submitMsg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
