b05ba7e7fdf4f5ac2426da9dfacfc556fc0daf3c
Andres Choque <80722893+andreschoque3@users.noreply.github.com>
2025-03-27 22:52:27 -0400

    changes 3/27

--- client/src/Pages/Coaching/Coaching.jsx
+++ client/src/Pages/Coaching/Coaching.jsx
@@ -14,7 +14,7 @@ function Coaching() {
         </div>
 
         <div className='google'>
-            <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1wI5fOFDs1LRCRu9v6eJ2BxMI3yhwY6AQBN8fp6jRfiR4vX8dfKFUfwYFxYApxXrrHR_84ctJY?gv=true" style={{ border:'0', width:"100%", height: '100vh'}}></iframe>
+            <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1wI5fOFDs1LRCRu9v6eJ2BxMI3yhwY6AQBN8fp6jRfiR4vX8dfKFUfwYFxYApxXrrHR_84ctJY?gv=true" title='calendar' style={{ border:'0', width:"100%", height: '100vh'}}></iframe>
         </div>
 
         <Footer/>
